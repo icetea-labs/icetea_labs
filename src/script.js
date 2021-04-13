506a589810fe408b16493ed0433e82c112e84459
chuoikhungbo <quanvantruong73@gmail.cp>
2021-04-13 15:44:55 +0700

    update slider

--- src/script.js
+++ src/script.js
@@ -1,80 +1,122 @@
-let scroll = 0;
-document.addEventListener('scroll', () => {
-  const navbar = document.querySelector('nav.navbar');
-  navbar.style.background = '#191919';
-  scroll++;
-  setTimeout(() => {
-    scroll--;
+import * as $ from 'jquery'
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/dist/css/splide.min.css'
+import './css/style.css'
+
+$(document).ready(function () {
+  let scroll = 0;
+  document.addEventListener('scroll', () => {
+    const navbar = document.querySelector('nav.navbar');
+    navbar.style.background = '#191919';
+    scroll++;
     setTimeout(() => {
-      if (document.documentElement.scrollTop === 0) navbar.style.background = '';
+      scroll--;
+      setTimeout(() => {
+        if (document.documentElement.scrollTop === 0) navbar.style.background = '';
+      }, 500);
     }, 500);
-  }, 500);
-})
-
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.3
-}
-
-function addFadeUpAnimation (entries, observer) {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('fadeUp')
-    }
   })
-}
 
-const observer = new IntersectionObserver(addFadeUpAnimation, options);
-const sections = document.querySelectorAll('.section')
-sections.forEach((section) => {
-  observer.observe(section)
-})
-
-function resizeSlider() {
-  const slider = document.querySelector('.slide-wrapper')
-  const wrapper = slider?.parentElement
-  if(slider.clientHeight > wrapper.clientHeight) {
-    wrapper.style.minHeight = slider.clientHeight + 'px'
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
   }
-}
 
-resizeSlider();
-
-window.addEventListener('resize', resizeSlider);
-
-let collapsed = true
-const navBtn = document.querySelector('.navbar-button')
-navBtn.addEventListener('click', () => {
-  const navMenu = document.querySelector('.navbar-menu')
-  const nav = document.querySelector('nav.navbar')
-  if(collapsed) {
-    nav.style.height = '100vh'
-    navMenu.classList.remove('collapsed')
-    navMenu.classList.add('opened')
-  } else {
-    nav.style.height = '52px'
-    navMenu.classList.remove('opened')
-    navMenu.classList.add('collapsed')
+  function addFadeUpAnimation (entries, observer) {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('fadeUp')
+      }
+    })
   }
-  collapsed = !collapsed
-})
 
-const navLink = document.querySelectorAll('.navbar-menu_item a')
-navLink.forEach(link => {
-  const locationId = link.dataset.location
-  link.addEventListener('click', () => {
-    let timeOut = 0
-    if(!collapsed) {
-      navBtn.click()
-      timeOut = 300
+  const observer = new IntersectionObserver(addFadeUpAnimation, options);
+  const sections = document.querySelectorAll('.section')
+  sections.forEach((section) => {
+    observer.observe(section)
+  })
+
+  function resizeSlider() {
+    const slider = document.querySelector('.slide-wrapper')
+    const wrapper = slider?.parentElement
+    if(slider.clientHeight > wrapper.clientHeight) {
+      wrapper.style.minHeight = slider.clientHeight + 'px'
     }
-    const location = document.querySelector(locationId)
+  }
+
+
+  window.addEventListener('resize', resizeSlider);
+
+  let collapsed = true
+  const navBtn = document.querySelector('.navbar-button')
+  navBtn.addEventListener('click', () => {
+    const navMenu = document.querySelector('.navbar-menu')
+    const nav = document.querySelector('nav.navbar')
+    if(collapsed) {
+      nav.style.height = '100vh'
+      navMenu.classList.remove('collapsed')
+      navMenu.classList.add('opened')
+    } else {
+      nav.style.height = '52px'
+      navMenu.classList.remove('opened')
+      navMenu.classList.add('collapsed')
+    }
+    collapsed = !collapsed
+  })
+
+  const navLink = document.querySelectorAll('.navbar-menu_item a')
+  navLink.forEach(link => {
+    const locationId = link.dataset.location
+    link.addEventListener('click', () => {
+      let timeOut = 0
+      if(!collapsed) {
+        navBtn.click()
+        timeOut = 300
+      }
+      const location = document.querySelector(locationId)
+      setTimeout(() => {
+        location.scrollIntoView( {behavior: 'smooth'})
+      }, timeOut)
+    })
+  })
+
+  const isMobileReso = window.innerWidth <= 600
+
+  const splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: isMobileReso ? 1 : 3,
+    perMove: isMobileReso ? 1 : 3,
+    pagination: false,
+    autoplay: true,
+    interval: 8000
+  })
+  splide.on('mounted', function () {
     setTimeout(() => {
-      location.scrollIntoView( {behavior: 'smooth'})
-    }, timeOut)
+      resizeSlider()
+    }, 300)
+    if(isMobileReso) {
+      document.querySelector('.delimiter__mobile').childNodes[0].classList.add('selected')
+    } else {
+      document.querySelector('.delimiter').childNodes[0].classList.add('selected')
+    }
+  })
+  splide.mount();
+  splide.on('move', function (newIndex) {
+    let index, delimiter
+    if(isMobileReso) {
+      delimiter = document.querySelector('.delimiter__mobile')
+      index = newIndex
+    } else {
+      delimiter = document.querySelector('.delimiter')
+      index = newIndex / 3
+    }
+    const items = delimiter.childNodes
+    items.forEach(item => {
+      item.classList.remove('selected')
+    })
+    items[index].classList.add('selected')
   })
 })
-
 
 
---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, 'src')
+}
+
+module.exports = {
+    mode: 'production',
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
+        assetModuleFilename: '[name][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(woff|woff2|otf|eot|ttf)$/,
+                use: {
+                    loader: 'url-loader'
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                loader: ImageMinimizerPlugin.loader,
+                options: {
+                    severityError: 'warning', // Ignore errors on corrupted images
+                    minimizerOptions: {
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            // ['optipng', { optimizationLevel: 5 }],
+                            // [
+                            //     'svgo',
+                            //     {
+                            //         plugins: [
+                            //             {
+                            //                 removeViewBox: false,
+                            //             },
+                            //         ],
+                            //     },
+                            // ],
+                        ],
+                    },
+                },
+
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[name].css'
+        }),
+        // new PurgeCSSPlugin({
+        //     paths: glob.sync(`${PATHS.src}/**/*.html`,  { nodir: true }),
+        // }),
+        new HtmlWebpackPlugin({
+            title: 'PolkaFoundry',
+            template: path.resolve(__dirname, 'src/index.html'), // template file
+            filename: 'index.html', // output file
+            favicon: path.resolve(__dirname, 'src/images/favicon.svg')
+        }),
+    ],
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            }
+        },
+    },
+};
