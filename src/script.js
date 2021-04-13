550b6efbf179ae4fd7f558972bb84f0b4363ce52
chuoikhungbo <quanvantruong73@gmail.cp>
2021-04-13 11:03:58 +0700

    update

--- src/script.js
+++ src/script.js
@@ -35,10 +35,46 @@ function resizeSlider() {
   const slider = document.querySelector('.slide-wrapper')
   const wrapper = slider?.parentElement
   if(slider.clientHeight > wrapper.clientHeight) {
-    wrapper.style.height = slider.clientHeight + 'px'
+    wrapper.style.minHeight = slider.clientHeight + 'px'
   }
 }
 
 resizeSlider();
 
-window.addEventListener('resize', resizeSlider)
+window.addEventListener('resize', resizeSlider);
+
+let collapsed = true
+const navBtn = document.querySelector('.navbar-button')
+navBtn.addEventListener('click', () => {
+  const navMenu = document.querySelector('.navbar-menu')
+  const nav = document.querySelector('nav.navbar')
+  if(collapsed) {
+    nav.style.height = '100vh'
+    navMenu.classList.remove('collapsed')
+    navMenu.classList.add('opened')
+  } else {
+    nav.style.height = '52px'
+    navMenu.classList.remove('opened')
+    navMenu.classList.add('collapsed')
+  }
+  collapsed = !collapsed
+})
+
+const navLink = document.querySelectorAll('.navbar-menu_item a')
+navLink.forEach(link => {
+  const locationId = link.dataset.location
+  link.addEventListener('click', () => {
+    let timeOut = 0
+    if(!collapsed) {
+      navBtn.click()
+      timeOut = 300
+    }
+    const location = document.querySelector(locationId)
+    setTimeout(() => {
+      location.scrollIntoView( {behavior: 'smooth'})
+    }, timeOut)
+  })
+})
+
+
+
